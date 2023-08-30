9dfdad5d19f42ba69e006c9541cdf02439e70410
Gatanot <nesap123@qq.com>
2023-08-30 16:09:12 +0800

    Change230830.2

--- 1002.c
+++ 1002.c
@@ -1,35 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
 void copy_arr(double j[], double m[], int n);
 void copy_ptr(double j[], double m[], int n);
-void copy_ptrs(double j[], double m[], double* n);
+void copy_ptrs(double j[], double m[], double *n);
 int main(void)
 {
-    double sourse[5] = {1.1,2.2,3.3,4.4,5.5};
+    double sourse[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
     double target1[5], target2[5], target3[5];
-    copy_arr(target3, sourse, 5);
+    copy_arr(target1, sourse, 5);
+    copy_ptr(target2, sourse, 5);
+    copy_ptrs(target3, sourse, &sourse[4]);
     for (int i = 0; i < 5; i++)
     {
         printf("%f \t%f \t%f\n", target1[i], target2[i], target3[i]);
     }
-    copy_ptr(target2, sourse, 5);
-    copy_ptrs(target3, sourse, &sourse[4]);
 }
-void copy_arr(double j[], double m[], int n){
+void copy_arr(double j[], double m[], int n)
+{
     for (int i = 0; i < n; i++)
     {
         j[i] = m[i];
     }
 }
-void copy_ptr(double j[], double m[], int n){
+void copy_ptr(double j[], double m[], int n)
+{
     for (int i = 0; i < n; i++)
     {
-        *(j+i) = *(m+i);
+        *(j + i) = *(m + i);
     }
-    
 }
-void copy_ptrs(double j[], double m[], double* n){
+void copy_ptrs(double j[], double m[], double *n)
+{
     for (int i = 0; i < 5; i++)
     {
-        *(j+4-i) = *(n-i);
+        *(j + 4 - i) = *(n - i);
     }
 }
