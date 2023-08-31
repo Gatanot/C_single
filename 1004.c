81c12123f9b3c461367fbf4e2e5830ffb83a796c
Gatanot <nesap123@qq.com>
2023-08-31 16:59:03 +0800

    Change230831.02

--- 1004.c
+++ 1004.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
 
+int numcount(char ch);
 int main(void)
 {
-    int ch = 0, leftcount = 0, rightcount = 0;
-    while ((ch = getchar()) != '@')
+    int ch = 0, sum = 0;
+    while ((ch = getchar()) != '\t')
     {
-        if (ch == '(')
-        {
-            leftcount += 1;
-        }
-        else if (ch == ')')
-        {
-            rightcount += 1;
-        }
-    }
-    if (leftcount == rightcount)
-    {
-        printf("YES");
-    }
-    else
-    {
-        printf("NO");
+        sum += numcount((char)ch)
     }
 
+    printf("%d", sum);
+
     return 0;
+}
+
+int numcount(char ch)
+{
+    int num = 0;
+    if (ch == 'a' || ch == 'd' || ch == 'g' || ch == 'j' || ch == 'm' || ch == 'p' || ch == 't' || ch == 'w' || ch == ' ')
+    {
+        num = 1;
+    }
+    else if (ch == 'b' || ch == 'e' || ch == 'h' || ch == 'k' || ch == 'n' || ch == 'q' || ch == 'u' || ch == 'x')
+    {
+        num = 2;
+    }
+    else if (ch == 'c' || ch == 'f' || ch == 'i' || ch == 'l' || ch == 'o' || ch == 'q' || ch == 'v' || ch == 'y')
+    {
+        num = 3
+    }
+    else if (ch == 's' || ch == 'z')
+    {
+        num = 4;
+    }
+
+    return num;
 }
--- test.c
+++ test.c
@@ -1,73 +1,38 @@
 #include <stdio.h>
-int judge(int numlist[]);
+
+int numcount(char ch);
 int main(void)
 {
-    int numlist[9];
-    int num = 0, j = 0;
-    for (int i = 0; i < 9; i++)
+    int ch = 0, sum = 0;
+    while ((ch = getchar()) != '\t')
     {
-        numlist[i] = (-i);
+        sum += numcount((char)ch)
     }
-    while ((num = getchar()) != '\t')
-    {
-        if (j % 2 == 0)
-        {
-            numlist[num] = 1;
-        }
-        else
-        {
-            numlist[num] = 2;
-        }
-        j++;
-        if (judge(&numlist[0]) == 1)
-        {
-            printf("xiaoa wins");
-            break;
-        }
-        else if (judge(&numlist[0]) == 2)
-        {
-            printf("uim wins");
-            break;
-        }
-    }
+
+    printf("%d", sum);
 
     return 0;
 }
-int judge(int numlist[])
+
+int numcount(char ch)
 {
-    int ReturnNum = 0;
-    if ((numlist[0] == numlist[1]) && (numlist[1] == numlist[2]))
+    int num = 0;
+    if (ch == 'a' || ch == 'd' || ch == 'g' || ch == 'j' || ch == 'm' || ch == 'p' || ch == 't' || ch == 'w' || ch == ' ')
     {
-        ReturnNum = numlist[0];
+        num = 1;
     }
-    else if ((numlist[3] == numlist[4]) && (numlist[4] == numlist[5]))
+    else if (ch == 'b' || ch == 'e' || ch == 'h' || ch == 'k' || ch == 'n' || ch == 'q' || ch == 'u' || ch == 'x')
     {
-        ReturnNum = numlist[3];
+        num = 2;
     }
-    else if ((numlist[6] == numlist[7]) && (numlist[7] == numlist[8]))
+    else if (ch == 'c' || ch == 'f' || ch == 'i' || ch == 'l' || ch == 'o' || ch == 'q' || ch == 'v' || ch == 'y')
     {
-        ReturnNum = numlist[6];
+        num = 3
     }
-    else if ((numlist[0] == numlist[4]) && (numlist[4] == numlist[8]))
+    else if (ch == 's' || ch == 'z')
     {
-        ReturnNum = numlist[0];
-    }
-    else if ((numlist[2] == numlist[4]) && (numlist[4] == numlist[6]))
-    {
-        ReturnNum = numlist[2];
-    }
-    else if ((numlist[0] == numlist[3]) && (numlist[3] == numlist[6]))
-    {
-        ReturnNum = numlist[0];
-    }
-    else if ((numlist[1] == numlist[4]) && (numlist[4] == numlist[7]))
-    {
-        ReturnNum = numlist[1];
-    }
-    else if ((numlist[2] == numlist[5]) && (numlist[5] == numlist[8]))
-    {
-        ReturnNum = numlist[2];
+        num = 4;
     }
 
-    return ReturnNum;
+    return num;
 }
