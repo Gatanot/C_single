533683d90ea0c6982a1ff2f62ab0f76f9387b824
Gatanot <nesap123@qq.com>
2023-09-29 23:29:18 +0800

    Change.23.09.29

--- test.c
+++ test.c
@@ -2,35 +2,9 @@
 
 int main(void)
 {
-    int m, n;
-    int sum = 0;
-    scanf("%d %d", &m, &n);
-    int numlist[m][n];
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            scanf("%d", &numlist[i][j]);
-        }
-    }
-    for (int i = 0; i < n; i++)
-    {
-        sum += numlist[0][i];
-    }
-    for (int i = 0; i < n; i++)
-    {
-        sum += numlist[m - 1][i];
-    }
-    for (int i = 1; i < m - 1; i++)
-    {
-        sum += numlist[i][0];
-    }
-    for (int i = 1; i < m - 1; i++)
-    {
-        sum += numlist[i][n - 1];
-    }
-
-    printf("%d", sum);
-
+    double r;
+    r /= 2;
+    scanf("%lf", &r);
+    printf("%.5lf", (4* 3.14 * r * r * r)/3);
     return 0;
 }
