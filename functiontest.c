cd79c39eb55323939de08ecc44ba814a96fd727e
Gatanot <nesap123@qq.com>
2023-10-05 09:58:54 +0800

    10.05.01

---
+++ functiontest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int t;
+    scanf("%d",&t);
+    long numlist[t];
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%ld",&numlist[i]);
+    }
+    
+    return 0;
+}
