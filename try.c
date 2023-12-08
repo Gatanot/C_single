d1cd6d490797c0b958b278071833f75e850d3be9
Gatanot <nesap123@qq.com>
2023-12-08 14:33:09 +0800

    23.12.09.01

--- try.c
+++ try.c
@@ -1,20 +1,4 @@
-#include <stdio.h>
-
-int main(void)
-{
-    char ch[10000];
-    char cha = 'a';
-    int i = 0;
-    while (cha = getchar())
-    {
-        ch[i] = cha;
-        i++;
-    }
-    i--;
-    for (; i >= 0; i--)
-    {
-        putchar(ch[i]);
-    }
-
-    return 0;
+#include<stdio.h>
+int main(void){
+    
 }
